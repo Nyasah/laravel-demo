52d818a556c39e0b4e076c3faace17fe6ebddb07
bharathisolanki <75109416+bharathisolanki@users.noreply.github.com>
2021-02-03 12:56:42 +0530

    Update welcome.blade.php
    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,6 +90,7 @@
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://forge.laravel.com">Forge21</a>
                     <a href="https://github.com/laravel/laravel">GitHub information</a>
                     <a href="https://github.com/laravel/laravel">About</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
