6b80b74b8c49ab4a7c21ee02eb439a7a1b3a9432
nlschemid <natascha@nyasah.net>
2021-02-05 12:19:29 +1100

    Change to title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Programming
                 </div>
 
                 <div class="links">
